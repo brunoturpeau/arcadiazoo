797211851fae909c4018fcb8d11cd15527c8c9f9
Bruno Turpeau <brunoturpeau99@gmail.com>
2024-05-13 09:44:50 +0200

    feat: add Avis page

---
+++ app/constants.ts
@@ -0,0 +1,20 @@
+export const COMMENTS_LIST = [
+    {
+        id: '1',
+        pseudo: 'Famille Campion',
+        comment: 'Une expérience extraordinaire ! Le zoo offre une immersion totale dans la vie sauvage. Les animations éducatives et la proximité avec les animaux sont incomparables. Une journée inoubliable pour les petits et les grands !',
+        date: '10/08/2024'
+    },
+    {
+        id: '2',
+        pseudo: 'Rocket 79',
+        comment: 'Quelle façon fantastique de visiter le zoo en famille ! Le petit train nous a permis de découvrir tous les recoins du parc sans fatigue. Les enfants étaient ravis de voir les animaux de si près. Une expérience vraiment mémorable pour toute la famille !',
+        date: '10/08/2024'
+    },
+    {
+        id: '3',
+        pseudo: 'PapyGato',
+        comment: 'Une visite enrichissante et écologique ! Ce zoo se distingue par son engagement envers la conservation et l\'éducation environnementale. Les habitats naturels et les programmes de préservation sont remarquables. ',
+        date: '10/08/2024'
+    },
+]
