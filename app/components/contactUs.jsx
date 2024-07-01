ff9008fd1d549ebacc108c648eca4a1f34c2afd0
Bruno Turpeau <brunoturpeau99@gmail.com>
2024-07-01 12:06:15 +0200

    styles: contactForm

--- app/components/contactUs.jsx
+++ app/components/contactUs.jsx
@@ -1,13 +1,22 @@
 "use client"
 
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 export const ContactUs = () => {
     const form = useRef();
+    const [statusName, setStatusName] = useState('initial')
+    const [contentName, setContentName] = useState('')
+    const [statusEmail, setStatusEmail] = useState('initial')
+    const [contentEmail, setContentEmail] = useState('')
+    const [statusMessage, setStatusMessage] = useState('initial')
+    const [contentMessage, setContentMessage] = useState('')
+    const [sending, setSending] = useState('')
+    const [msg, setMsg] = useState('')
+
 
     const sendEmail = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
         emailjs
             .sendForm('service_d3gpajb', 'template_a24ukvs', form.current, {
@@ -15,23 +24,33 @@ export const ContactUs = () => {
             })
             .then(
                 () => {
-                    console.log('SUCCESS!');
+                    setSending('sent')
+                    setMsg('Votre message a été envoyé avec succès !')
                 },
                 (error) => {
-                    console.log('FAILED...', error.text);
+                    console.log('FAILED...', error.text)
+                    setSending('error')
+                    setMsg('Une erreur est survenue !')
                 },
             );
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
-        </form>
-    );
-};
+        <div className="md:w-3/4 mx-auto">
+            <form className="flex flex-col"
+                  ref={form}
+                  onSubmit={sendEmail}>
+                <label className={`mb-1`}>Votre nom</label>
+                <input className={`mb-3`} type="text" name="user_name"/>
+                <label className={`mb-1`}>Votre adresse email</label>
+                <input className={`mb-3`} type="email" name="user_email"/>
+                <label className={`mb-1`}>Votre message</label>
+                <textarea className={`mb-10`} name="message"/>
+                <input className={`btn-primary`} type="submit" value="Envoyer"/>
+                <div className={`mt-5 p-5 rounded-md ${sending == 'sent' ? 'bg-yellow-alt text-primary-dark' : ''} `}>
+                    {msg}
+                </div>
+            </form>
+        </div>
+    )
+}
