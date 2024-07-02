774f23db7479cadb835eaf91dc4532296b5dd822
Bruno Turpeau <brunoturpeau99@gmail.com>
2024-07-02 18:39:24 +0200

    feat: validate commentForm

--- app/components/comment-form.jsx
+++ app/components/comment-form.jsx
@@ -1,26 +1,68 @@
 'use client'
+import { useState } from 'react'
 export const CommentForm = () => {
+    const [statusPseudo, setStatusPseudo] = useState(null)
+    const [statusMessage, setStatusMessage] = useState(null)
+    const [contentPseudo, setContentPseudo] = useState('')
+    const [contentMessage, setContentMessage] = useState('')
+
+    function handlePseudoChange(e) {
+        const value = e.target.value
+        let valueLenght = value.length
+
+        if (valueLenght > 2 && valueLenght < 21) {
+            setStatusPseudo('valid')
+            setContentPseudo(value)
+        } else {
+            setStatusPseudo('invalid')
+        }
+    }
+
+    function handleMessageChange(e) {
+        const value = e.target.value
+        if (value !== '') {
+            setStatusMessage('valid')
+            setContentMessage(contentMessage)
+        } else {
+            setStatusMessage('invalid')
+        }
+    }
     function sendComment(e){
         e.preventDefault()
-        confirm('Envoi du commentaire')
+        confirm('Envoi du commentaire de ' + contentPseudo )
     }
 
     return (
         <form onSubmit={sendComment}>
-            <label className={`block mt-10 mb-2`} htmlFor="pseudo">Pseudo
-                <input name={`pseudo`} className={`w-full mb-5`} type="text" required/>
-            </label>
-            <label className={`block mb-2`} htmlFor="commentaire">Commentaire
-                <textarea
-                    className={`w-full md:mb-2`}
-                    name="commentaire"
-                    id="inputCommentaire"
-                    cols={30}
-                    rows={5}
-                    required
-                ></textarea>
-            </label>
-            <div className="text-center">
+            <label className={`block mt-10 mb-2`} htmlFor="pseudo">Pseudo</label>
+            <input
+                name={`pseudo`}
+                onChange={handlePseudoChange}
+                className={`w-full mb-5 ${statusPseudo === 'valid' ? 'bg-success ' : ''} ${statusPseudo === 'invalid' ? 'bg-danger ' : ''}`}
+                type="text"
+                required
+            />
+            <p
+                className={`text-white mb-5 text-sm ${statusPseudo === 'invalid' ? '' : 'hidden'}`}
+            >
+                Le champ pseudo doit contenir entre 3 et 20 caractères
+            </p>
+            <label className={`block mb-2`} htmlFor="commentaire">Commentaire</label>
+            <textarea
+                className={`w-full md:mb-2 ${statusMessage === 'valid' ? 'bg-success' : ''} ${statusMessage === 'invalid' ? 'bg-danger' : ''}`}
+                onChange={handleMessageChange}
+                name="commentaire"
+                id="inputCommentaire"
+                cols={30}
+                rows={5}
+                required
+            ></textarea>
+            <p
+                className={`text-white mb-5 text-sm ${statusMessage === 'invalid' ? '' : 'hidden'}`}
+            >
+                Le champ message ne doit pas être vide.
+            </p>
+            <div className="text-center mt-5">
                 <button className={`btn-primary`}>Envoyer</button>
             </div>
         </form>
--- app/components/contactUs.jsx
+++ app/components/contactUs.jsx
@@ -6,8 +6,8 @@ import emailjs from '@emailjs/browser'
 export const ContactUs = () => {
     const form = useRef();
     const [statusName, setStatusName] = useState(null)
-    const [statusEmail, setStatusEmail] = useState('initial')
-    const [statusMessage, setStatusMessage] = useState('initial')
+    const [statusEmail, setStatusEmail] = useState(null)
+    const [statusMessage, setStatusMessage] = useState(null)
     const [sending, setSending] = useState('')
     const [msg, setMsg] = useState('')
 
@@ -73,14 +73,14 @@ export const ContactUs = () => {
                   onSubmit={sendEmail}>
                 <label className={`mb-1`}>Votre nom</label>
                 <input
-                    className={`mb-3 ${statusName == 'valid' ? 'bg-success ' : ''} ${statusName == 'invalid' ? 'bg-danger ' : ''}`}
+                    className={`mb-3 ${statusName === 'valid' ? 'bg-success ' : ''} ${statusName === 'invalid' ? 'bg-danger ' : ''}`}
                     onChange={handleNameChange}
                     type="text"
                     name="user_name"
                     required
                 />
                 <p
-                    className={`text-[#ef4444] mb-5 text-sm ${statusName == 'invalid' ? '' : 'hidden'}`}
+                    className={`text-[#ef4444] mb-5 text-sm ${statusName === 'invalid' ? '' : 'hidden'}`}
                 >
                     Le champ nom doit contenir entre 3 et 30 caractères
                 </p>
@@ -88,7 +88,7 @@ export const ContactUs = () => {
                     Votre adresse email
                 </label>
                 <input
-                    className={`mb-3 ${statusEmail == 'valid' ? 'bg-success ' : ''} ${statusEmail == 'invalid' ? 'bg-danger ' : ''}`}
+                    className={`mb-3 ${statusEmail === 'valid' ? 'bg-success ' : ''} ${statusEmail === 'invalid' ? 'bg-danger ' : ''}`}
                     onChange={handleEmailChange}
                     placeholder="exemple@email.com"
                     type="email"
@@ -96,26 +96,26 @@ export const ContactUs = () => {
                     required
                 />
                 <p
-                    className={`text-[#ef4444] mb-5 text-sm ${statusEmail == 'invalid' ? '' : 'hidden'}`}
+                    className={`text-[#ef4444] mb-5 text-sm ${statusEmail === 'invalid' ? '' : 'hidden'}`}
                 >
                     L&apos;adresse email n&apos;est pas valide
                 </p>
                 <label className={`mb-1`}>Votre message</label>
                 <textarea
-                    className={`mb-3 ${statusMessage == 'valid' ? 'bg-success' : ''} ${statusMessage == 'invalid' ? 'bg-danger' : ''}`}
+                    className={`mb-3 ${statusMessage === 'valid' ? 'bg-success' : ''} ${statusMessage === 'invalid' ? 'bg-danger' : ''}`}
                     onChange={handleMessageChange}
                     name="message"
                     required
                 />
                 <p
-                    className={`text-[#ef4444] mb-5 text-sm ${ statusMessage == 'invalid' ? '' : 'hidden'}`}
+                    className={`text-[#ef4444] mb-5 text-sm ${ statusMessage === 'invalid' ? '' : 'hidden'}`}
                 >
                     Le champ message ne doit pas être vide.
                 </p>
                 <div className="flex">
                     <input className={`btn-primary w-[150px] mt-5`} type="submit" value="Envoyer"/>
                 </div>
-                <div className={`mt-5 p-5 rounded-md text-primary-dark ${sending == 'sent' ? 'bg-success' : ''} ${sending == 'error' ? 'bg-danger' : ''} `}>
+                <div className={`mt-5 p-5 rounded-md text-primary-dark ${sending === 'sent' ? 'bg-success' : ''} ${sending === 'error' ? 'bg-danger' : ''} `}>
                     {msg}
                 </div>
             </form>
